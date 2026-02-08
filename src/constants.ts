3be8f9033a730ae346c57716d4b2bcd07496fefd
0x7E1FA7ED <legoliu@outlook.com>
2026-02-09 00:14:10 +0800

    feat: add WeChat social link with modal popup interaction

--- src/constants.ts
+++ src/constants.ts
@@ -7,6 +7,7 @@ import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
 import IconFacebook from "@/assets/icons/IconFacebook.svg";
 import IconTelegram from "@/assets/icons/IconTelegram.svg";
 import IconPinterest from "@/assets/icons/IconPinterest.svg";
+import IconWechat from "@/assets/icons/IconWechat.svg";
 import { SITE } from "@/config";
 
 interface Social {
@@ -40,6 +41,12 @@ export const SOCIALS: Social[] = [
     href: "mailto:legoliu@outlook.com",
     linkTitle: `Send an email to ${SITE.title}`,
     icon: IconMail,
+  },
+  {
+    name: "WeChat",
+    href: "#",
+    linkTitle: `Add ${SITE.title} on WeChat`,
+    icon: IconWechat,
   },
 ] as const;
 
